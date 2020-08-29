0528909e71208be440a85ee9c2f0dd082a7add6e
sqsong47 <768841936@qq.com>
2020-08-29 22:43:28 +0800

    Signed-off-by: sqsong47 <768841936@qq.com> 加入输出行

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <QCoreApplication>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    cout << "QT git" << endl;
     return a.exec();
 }
