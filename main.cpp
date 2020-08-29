340103f9ae639ae23544a9327d36f5355c1b7573
sqsong47 <768841936@qq.com>
2020-08-29 22:49:43 +0800

    Signed-off-by: sqsong47 <768841936@qq.com> 测试一个branch

--- main.cpp
+++ main.cpp
@@ -7,5 +7,9 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     cout << "QT git" << endl;
+
+
+    //    新增一个branch
+    cout << "Branch" << endl;
     return a.exec();
 }
